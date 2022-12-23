393d3817256e8af2f6777df6105b23e312a68e07
JakariaJishan <jakariajishan987@gmail.com>
2022-12-24 01:45:05 +0600

    Add some bug

---
+++ script.js
@@ -0,0 +1,12 @@
+const getIdFromUser = (a,b)=>{
+    if (a<6) {
+        return;
+        return a+b;
+    }
+}
+const getIdFromUser=(myObj){
+    return myObj;
+}
+
+getIdFromUser(4,6);
+getIdFromUser({title:'hide and seek'})
